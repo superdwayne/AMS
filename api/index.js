e36b4fdfa0bfdb3e55f7cccfd9bbebc345354b49
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2023-07-05 15:37:21 +0200

    Test changes

--- api/index.js
+++ api/index.js
@@ -6,79 +6,64 @@ const cheerio = require('cheerio');
 const app = express();
 app.use(cors());
 
+// Weather API route
+app.get('/weather', async (req, res, next) => {
+  try {
+    const location = 'London'; // replace with your desired location
+    const apiKey = '7a8fd7f7876cb914ed733ae548f7d6f9'; // replace with your API key
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
 
+    const response = await axios.get(url);
+    const data = response.data;
+    const weatherData = {
+      cityName: data.name,
+      currentTemp: data.main.temp,
+      weatherDescription: data.weather[0].description,
+      coordslon: data.coord.lon,
+      coordslat: data.coord.lat
+    };
 
-// const API_KEY = '3eec716e0bec7d84a3d2f87e39b842d8'; // replace with your API key
-// const location = 'London'; // replace with your desired location
-
-// axios.get(`http://api.weatherstack.com/current?access_key=${API_KEY}&query=${location}`)
-//   .then(response => {
-//     console.log(response.data);
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
-
-app.get('/weather', (req, res) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=7a8fd7f7876cb914ed733ae548f7d6f9&units=metric`;
-
-  axios.get(url)
-    .then(response => {
-      const data = response.data;
-      // console.log(data)
-      const weatherData = {
-        cityName: data.name,
-        currentTemp: data.main.temp,
-        weatherDescription: data.weather[0].description,
-        coordslon: data.coord.lon,
-        coordslat: data.coord.lat
-        
-      };
-     
-
-      res.send({ weatherData });
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).json({ message: 'An error occurred while fetching data' });
-    });
+    res.json({ weatherData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while fetching data' });
+  }
 });
 
-app.get('/who', (req, res) => {
-  const url = 'https://www.who.int/'; // replace with the actual URL
+// WHO website scraping route
+app.get('/who', async (req, res, next) => {
+  try {
+    const url = 'https://www.who.int/'; // replace with the actual URL
 
-  axios(url)
-    .then(response => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      
-      const statsTable = $('.section-heading'); // replace with actual selector
-      const images = [];
-      
-      $('.mdc-card__media').find('img').each((index, element) => {
+    const response = await axios(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const statsTable = $('.section-heading'); // replace with actual selector
+    const images = [];
+
+    $('.mdc-card__media')
+      .find('img')
+      .each((index, element) => {
         const src = $(element).attr('src');
         images.push(src);
       });
-      //console.log(images);
 
-      const stats = [];
+    const stats = statsTable.map(function () {
+      const title = $(this).find('h2').text();
+      return {
+        title,
+        images
+      };
+    }).get();
 
-      statsTable.each(function () {
-        const title = $(this).find('h2').text();
-
-        stats.push({
-          title,
-          images
-        });
-      });
-
-      res.json({ stats, images });
-     // console.log(stats);
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).json({ message: 'An error occurred while scraping data' });
-    });
+    res.json({ stats, images });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while scraping data' });
+  }
 });
 
-app.listen(3002, () => console.log('Server listening on port 3002'));
+// Start the server
+const port = 3002;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,54 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import { Parallax } from 'react-scroll-parallax'
-import { ParallaxBanner } from "react-scroll-parallax";
-import { useParallax } from "react-scroll-parallax";
+import { Parallax, ParallaxBanner } from 'react-scroll-parallax';
 import axios from 'axios';
 import { Player } from 'video-react';
 import './App.css';
 
-const apiUrl = process.env.REACT_APP_API_URL;
-
-const birdWeatherVideo = './video/birds.mp4';
-const coldWeatherVideo = './video/grey.mp4';
-const warmWeatherVideo  = './video/fff.mp4';
-
-const warmWeatherImage = 'https://images.unsplash.com/photo-1517495306984-f84210f9daa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'
-const coldWeatherImage = 'https://images.unsplash.com/photo-1548266652-99cf27701ced?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
-
-
-
 function Weather() {
-  
   const [weatherData, setWeatherData] = useState(null);
   const [backgroundVideo, setBackgroundVideo] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get(`${apiUrl}/weather`)
+    const apiUrl = process.env.REACT_APP_API_URL;
+    axios
+      .get(`http://localhost:3002/weather`)
       .then(response => {
         setWeatherData(response.data.weatherData);
-        console.log(response.data.weatherData.currentTemp)
+        console.log(response.data.weatherData.currentTemp);
       })
       .catch(error => {
         console.error(error);
+        setError('An error occurred while fetching weather data.');
       });
   }, []);
 
   useEffect(() => {
     if (weatherData) {
       if (weatherData.currentTemp < 20) {
-        setBackgroundVideo(coldWeatherVideo);
+        setBackgroundVideo('./video/grey.mp4');
       } else {
-        setBackgroundVideo(warmWeatherVideo);
+        setBackgroundVideo('./video/fff.mp4');
       }
     }
   }, [weatherData]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   if (!weatherData) {
     return <div>Loading...</div>;
   }
 
   return (
-    <>    
+    <>
       <Player
         playsInline
         autoPlay
@@ -58,65 +52,86 @@ function Weather() {
         className="background-video"
       />
 
-    <section className='wrapper'>
-      <img className='logo' src='./Logo_Overview_bk.png' alt='AMQ' style={{width: '152px'}} />
+      <section className="wrapper">
+        <img
+          className="logo"
+          src="./Logo_Overview_bk.png"
+          alt="AMQ"
+          style={{ width: '152px' }}
+        />
 
-      <span className='weatherData'>  
-        <h1>{weatherData.cityName}</h1>
-        <p>{weatherData.coordslon} {weatherData.coordslat}</p>
-      </span> 
-      </section> 
-
+        <span className="weatherData">
+          <h1>{weatherData.cityName}</h1>
+          <p>
+            {weatherData.coordslon} {weatherData.coordslat}
+          </p>
+        </span>
+      </section>
     </>
   );
 }
 
 function App() {
-
-  // const parallax = useParallax({
-  //   rotate: [0, 360],
-  // });
-
   return (
-      <div className="App">
-        <section className="App-header">
-       
-          <Weather />
-        </section>
-        <section className="container-body">
-          <ParallaxBanner
+    <div className="App">
+      <section className="App-header">
+        <Weather />
+      </section>
+      <section className="container-body">
+        <ParallaxBanner
           layers={[
-          { image: 'https://static.dezeen.com/uploads/2022/10/alexander-mcqueen-smiljan-radic-inflatable-dome-spring-summer-2023-fashion_dezeen_2364_col_0-scaled.jpg', speed: -20 },
-        ]}
-        className="aspect-[2/1]"
-      >
-        <div className="absolute inset-0 flex items-center justify-center" style={{height: '100vh'}}>
-          {/* <h1 className="text-8xl text-white font-thin">Hello World!</h1> */}
-        </div>
+            {
+              image:
+                'https://static.dezeen.com/uploads/2022/10/alexander-mcqueen-smiljan-radic-inflatable-dome-spring-summer-2023-fashion_dezeen_2364_col_0-scaled.jpg',
+              speed: -20,
+            },
+          ]}
+          className="aspect-[2/1]"
+        >
+          <div
+            className="absolute inset-0 flex items-center justify-center"
+            style={{ height: '100vh' }}
+          >
+            {/* <h1 className="text-8xl text-white font-thin">Hello World!</h1> */}
+          </div>
         </ParallaxBanner>
 
         <ParallaxBanner
           layers={[
-          { image: 'https://images.unsplash.com/photo-1521128688982-60a2f18654d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80', speed: -20 },
-        ]}
-        className="aspect-[2/1]"
-      >
-        <div className="absolute inset-0 flex items-center justify-center" style={{height: '200vh'}}>
-          
-        </div>
+            {
+              image:
+                'https://images.unsplash.com/photo-1521128688982-60a2f18654d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80',
+              speed: -20,
+            },
+          ]}
+          className="aspect-[2/1]"
+        >
+          <div
+            className="absolute inset-0 flex items-center justify-center"
+            style={{ height: '200vh' }}
+          ></div>
         </ParallaxBanner>
         <ParallaxBanner
           layers={[
-          { image: 'https://images.unsplash.com/photo-1602934585418-f588bea4215c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80', speed: -20 },
-        ]}
-        className="aspect-[2/1]"
-      >
-        <div className="absolute inset-0 flex items-center justify-center" style={{height: '200vh'}}>
-          <h1 className="text-8xl text-white font-thin">"Extraordinarily beautiful and infinitely adaptable, the orchid mimics both predator and prey." <br /> -- Sarah Burton</h1>
-        </div>
+            {
+              image:
+                'https://images.unsplash.com/photo-1602934585418-f588bea4215c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80',
+              speed: -20,
+            },
+          ]}
+          className="aspect-[2/1]"
+        >
+          <div
+            className="absolute inset-0 flex items-center justify-center"
+            style={{ height: '200vh' }}
+          >
+            <h1 className="text-8xl text-white font-thin">
+              "Extraordinarily beautiful and infinitely adaptable, the orchid mimics both predator and prey." <br /> -- Sarah Burton
+            </h1>
+          </div>
         </ParallaxBanner>
       </section>
-      </div>
+    </div>
   );
 }
 
